e1d5c4c6371f3dc8983aae73ed633b396b93649e
Simon Padron <89358707+simonp46@users.noreply.github.com>
2021-11-05 16:00:44 -0500

    Add project files.

---
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -0,0 +1,74 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+             //Buscar estudiantes 
+             if (context.Students.Any())
+            {
+                return; //Creación de la base de datos
+            }
+
+            var students = new Student[]
+               {new Student{FirstMidName="Elon",LastName="Musk",EnrollmentDate=DateTime.Parse("2005-09-01")},
+            new Student{FirstMidName="Jeff",LastName="Bezos",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Alan",LastName="Turing",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Bill",LastName="Gates",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Mark",LastName="Zuckerberg",EnrollmentDate=DateTime.Parse("2002-09-01")},
+            new Student{FirstMidName="Steve",LastName="Jobs",EnrollmentDate=DateTime.Parse("2001-09-01")},
+            new Student{FirstMidName="Sundar",LastName="Pichai",EnrollmentDate=DateTime.Parse("2003-09-01")},
+            new Student{FirstMidName="Jack",LastName="Dorsey",EnrollmentDate=DateTime.Parse("2005-09-01")}
+           };
+            foreach (Student s in students)
+            {
+                context.Students.Add(s);
+            }
+            context.SaveChanges();
+
+            var courses = new Course[]
+            {
+            new Course{CourseID=1050,Title="Quimica",Credits=3},
+            new Course{CourseID=4022,Title="Microeconomia",Credits=3},
+            new Course{CourseID=4041,Title="Macroeconomia",Credits=3},
+            new Course{CourseID=1045,Title="Calculo",Credits=4},
+            new Course{CourseID=3141,Title="Trigonometria",Credits=4},
+            new Course{CourseID=2021,Title="Composicion",Credits=3},
+            new Course{CourseID=2042,Title="Literatura",Credits=4}
+            };
+            foreach (Course c in courses)
+            {
+                context.Courses.Add(c);
+            }
+            context.SaveChanges();
+
+            var enrollments = new Enrollment[]
+            {
+            new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+            new Enrollment{StudentID=1,CourseID=4022,Grade=Grade.C},
+            new Enrollment{StudentID=1,CourseID=4041,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=1045,Grade=Grade.B},
+            new Enrollment{StudentID=2,CourseID=3141,Grade=Grade.F},
+            new Enrollment{StudentID=2,CourseID=2021,Grade=Grade.F},
+            new Enrollment{StudentID=3,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=1050},
+            new Enrollment{StudentID=4,CourseID=4022,Grade=Grade.F},
+            new Enrollment{StudentID=5,CourseID=4041,Grade=Grade.C},
+            new Enrollment{StudentID=6,CourseID=1045},
+            new Enrollment{StudentID=7,CourseID=3141,Grade=Grade.A},
+            };
+            foreach (Enrollment e in enrollments)
+            {
+                context.Enrollments.Add(e);
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ContosoUniversity/Startup.cs
@@ -0,0 +1,65 @@
+using ContosoUniversity.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // agregar servicios al contenedor.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddControllersWithViews();
+        }
+
+        // Utilice este método para configurar la canalización de solicitudes HTTP.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+    }
+}
